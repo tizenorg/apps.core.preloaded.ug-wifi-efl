58735f4353c42f66b9fa78ff7721edc9f8f80371
Seonah Moon <seonah1.moon@samsung.com>
2016-07-25 09:06:05 +0900

    Fixed wrong function call
    Change-Id: I6501e4398c3d0eda5488f95ef15ae123b73ba589
    Signed-off-by: Seonah Moon <seonah1.moon@samsung.com>

--- sources/wearable/src/wearable-circle/app_main.c
+++ sources/wearable/src/wearable-circle/app_main.c
@@ -991,8 +991,8 @@ static layout_detail_object *_detail_create(app_object *app_obj, wifi_ap_object 
 	layout_detail_set_menu_cb(detail_obj, DETAIL_MENU_IP,
 				  __detail_menu_ip_text_get_cb, NULL, NULL, NULL, app_obj);
 	/* Add empty item to avoid overlap */
-	layout_ap_info_set_menu_cb(detail_obj, DETAIL_MENU_EMPTY,
-				   NULL, NULL, NULL, NULL, NULL, NULL);
+	layout_detail_set_menu_cb(detail_obj, DETAIL_MENU_EMPTY,
+				   NULL, NULL, NULL, NULL, NULL);
 
 
 	if (is_removable_ap) {
